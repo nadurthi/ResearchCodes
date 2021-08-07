24513956617f516ba201bc1ae3687bca5b424c9d
Nagavenkat Adurthi <n.adurthi@gmail.com>
2021-08-06 19:18:54 -0500

    gg

--- consumer_producer.py
+++ consumer_producer.py
@@ -8,43 +8,49 @@ import multiprocessing as mp
 import queue
 import time
 
-def consumer(ExitFlag,inQ,outQ):
-    while(True):
-        lines = None
-        try:
-            lines = inQ.get(True,0.2)
-        except queue.Empty:
-            lines = None
-        
-        if lines is not None:
-            # do some work
-            result = 1
-            outQ.put(result)
-            print("Working on ",lines)
-            time.sleep(1)
-            pass
-        
-        if (ExitFlag.is_set() and inQ.empty()):
-            print("exiting producer")
-            break
-        
 
 
-inQ = mp.Queue(5) # mi took the max size of queue as 50 here
-outQ = mp.Queue()
-ExitFlag = mp.Event()
-ExitFlag.clear()
 
-processes = []
-Ncore = 4
-for i in range(Ncore):
-    p = mp.Process(target=consumer, args=(ExitFlag,inQ,outQ))
-    processes.append( p )
-    p.start()
-
-time.sleep(3)
 
 if __name__=="__main__":
+    def consumer(ExitFlag,inQ,outQ):
+        print("in consumer")
+        while(True):
+            lines = None
+            try:
+                lines = inQ.get(True,0.2)
+            except queue.Empty:
+                lines = None
+            
+            if lines is not None:
+                # do some work
+                result = 1
+                outQ.put(result)
+                print("Working on ",lines)
+                time.sleep(1)
+                pass
+            
+            if (ExitFlag.is_set() and inQ.empty()):
+                print("exiting producer")
+                break
+            
+            
+    inQ = mp.Queue(5) # mi took the max size of queue as 50 here
+    outQ = mp.Queue()
+    ExitFlag = mp.Event()
+    ExitFlag.clear()
+    
+    processes = []
+    Ncore = 4
+    for i in range(Ncore):
+        p = mp.Process(target=consumer, args=(ExitFlag,inQ,outQ))
+        p.start()
+        processes.append( p )
+        
+    
+        time.sleep(1)
+    
+
     LinesSets=[1,2,3,4,5,6,7,8,9,10]
     for lines in LinesSets:
         if not inQ.full():
@@ -61,11 +67,18 @@ if __name__=="__main__":
         if inQ.empty():
             break
         else:
-            time.sleep(5)
+            time.sleep(2)
             
     ExitFlag.set()
-    time.sleep(5)
+    time.sleep(2)
     
+    while True:
+        res = outQ.get()
+        print(res)
+        
+        if inQ.empty():
+            break
+        
     for p in processes:
         p.join()
         
