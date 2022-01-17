e786821eff45188bbd2b4196fff2dc3178534270
Nagavenkat Adurthi <n.adurthi@gmail.com>
2022-01-17 13:34:34 -0600

    bin match works good

--- lidarprocessing/numba_codes/point2Dprocessing_numba.py
+++ lidarprocessing/numba_codes/point2Dprocessing_numba.py
@@ -318,7 +318,7 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
     mn[1] = np.min(X1[:,1])
     mx[0] = np.max(X1[:,0])
     mx[1] = np.max(X1[:,1])
-    rmax=np.max(np.sqrt(X2[:,0]**2+X2[:,1]**2))
+    # rmax=np.max(np.sqrt(X2[:,0]**2+X2[:,1]**2))
 
     P = mx-mn
 
@@ -374,9 +374,9 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
     HLevels=HLevels[::-1]
     HLevels=[np.ascontiguousarray(H).astype(np.int32) for H in HLevels]
     
-    P2=0.5*P
+    P2=0.1*P
     Lmax=P2*(np.floor(Lmax/P2)+1)
-    Lmax=np.maximum(Lmax,P)
+    # Lmax=np.maximum(Lmax,P)
     
     LmaxOrig=np.zeros(2,dtype=np.float64)
     LmaxOrig[0]=Lmax[0]
@@ -407,12 +407,17 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
         value_type=float_2Darray,
     )
     ii=0
+    R = np.array([[np.cos(thmax), -np.sin(thmax)],[np.sin(thmax), np.cos(thmax)]])
+    X222=np.transpose((R.T).dot(X2.T))
+    
     thfineRes = thmin
-    thL=np.arange(-thmax,thmax+thfineRes,thfineRes,dtype=np.float64)
+    thL=np.arange(0,2*thmax+thfineRes,thfineRes,dtype=np.float64)
+
     # X2=np.ascontiguousarray(X2)
-    for th in thL:
+    for j in range(len(thL)):
+        th=thL[j]
         R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
-        XX=np.transpose(R.dot(X2.T))
+        XX=np.transpose(R.dot(X222.T))
         Xth[th]=XX
         
         
@@ -421,7 +426,7 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
             Tj=np.array((d0,d1))
             Oj = np.array((xs,ys))
             cost2=getPointCost(H,dx,Xth[th],Oj,Tj)
-            h.append((-cost2-np.random.rand()/1000,xs,ys,d0,d1,lvl,th))
+            h.append((-cost2-np.random.rand()/1e10,xs,ys,d0,d1,lvl,th))
             
     heapq.heapify(h)
     
@@ -451,10 +456,10 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
             for ys in Yg[:2]:
                 Oj = np.array((xs,ys))
                 cost3=getPointCost(H,dx,Xth[th],Oj,Tj) 
-                heapq.heappush(h,(-cost3-np.random.rand()/1000,xs,ys,d0,d1,float(nlvl),th))
+                heapq.heappush(h,(-cost3-np.random.rand()/1e10,xs,ys,d0,d1,float(nlvl),th))
 
     t=mainSolbox[1:3]
-    th = mainSolbox[6]
+    th = mainSolbox[6]+np.pi
     cost=-mainSolbox[0]
     
 
@@ -484,7 +489,223 @@ def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
     H21comp=nplinalg.inv(H12comp)
     
     return H21comp
+
+@njit(cache=True)
+def binMatcherAdaptive4_good(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
+    #  X11 are global points
+    # X22 are points with respect to a local frame (like origin of velodyne)
+    # H12 takes points in the velodyne frame to X11 frame (global)
     
+    n=histsmudge =2 # how much overlap when computing max over adjacent hist for levels
+    H21comp=np.identity(3)
+        
+    mn=np.zeros(2)
+    mx=np.zeros(2)
+    mn_orig=np.zeros(2)
+    mn_orig[0] = np.min(X11[:,0])
+    mn_orig[1] = np.min(X11[:,1])
+    
+    mn_orig=mn_orig-dxMatch
+    
+    
+    # R=H12[0:2,0:2]
+    # t=H12[0:2,2]
+    # X222 = R.dot(X22.T).T+t
+    
+    H12mn = H12.copy()
+    H12mn[0:2,2]=H12mn[0:2,2]-mn_orig
+    
+    # X2=X222-mn_orig
+    X1=X11-mn_orig
+    
+    
+    # t2 = np.mean(X2,axis=0)
+    # X20 = X2-t2
+    
+    mn[0] = np.min(X1[:,0])
+    mn[1] = np.min(X1[:,1])
+    mx[0] = np.max(X1[:,0])
+    mx[1] = np.max(X1[:,1])
+    # rmax=np.max(np.sqrt(X2[:,0]**2+X2[:,1]**2))
+
+    P = mx-mn
+
+    mxlvl=0
+    dx0=mx+dxMatch
+    dxs = []
+    XYedges=[]
+    for i in range(0,100):
+        f=2**i
+        
+        xedges=np.linspace(0,mx[0]+1*dxMatch[0],f+1)
+        yedges=np.linspace(0,mx[1]+1*dxMatch[0],f+1)
+        XYedges.append((xedges,yedges))
+        dx=np.array([xedges[1]-xedges[0],yedges[1]-yedges[0]])
+    
+        dxs.append(dx)
+        
+        if np.any(dx<=dxMatch):
+            break
+        
+    mxlvl=len(dxs)
+    
+
+    dxs=[dx.astype(np.float64) for dx in dxs]
+
+    
+    
+    H1match=numba_histogram2D(X1, XYedges[-1][0],XYedges[-1][1])
+    H1match = np.sign(H1match)
+    
+    
+    
+    
+
+    levels=[]
+    HLevels=[H1match]
+    
+    
+    
+    
+    
+    for i in range(1,mxlvl):
+        
+    
+    
+        Hup = HLevels[i-1]
+        H=UpsampleMax(Hup,n)
+
+        HLevels.append(H)
+          
+    
+    mxLVL=len(HLevels)-1
+    HLevels=HLevels[::-1]
+    HLevels=[np.ascontiguousarray(H).astype(np.int32) for H in HLevels]
+    
+    # P2=0.001*P
+    # Lmax=P2*(np.floor(Lmax/P2)+1)
+    # Lmax=np.maximum(Lmax,P)
+    
+    LmaxOrig=np.zeros(2,dtype=np.float64)
+    LmaxOrig[0]=Lmax[0]
+    LmaxOrig[1]=Lmax[1]
+
+    SolBoxes_init=[]
+    # X2[:,0]=X2[:,0]-LmaxOrig[0]
+    # X2[:,1]=X2[:,1]-LmaxOrig[1]
+    
+    # print(Lmax)
+    # Lmax=dxs[-1]*(np.floor(Lmax/dxs[-1])+1)
+    # print(Lmax)
+    for xs in np.arange(-Lmax[0],Lmax[0],dxs[0][0]):
+        for ys in np.arange(-Lmax[1],Lmax[1],dxs[0][1]):
+            SolBoxes_init.append( (xs,ys,dxs[0][0],dxs[0][1]) )
+    
+    
+    
+    
+    
+    
+    h=[(100000.0,0.0,0.0,0.0,0.0,0.0,0.0)]
+    lvl=0
+    dx=dxs[lvl]
+    H=HLevels[lvl]
+    
+    Xth= Dict.empty(
+        key_type=types.float64,
+        value_type=float_2Darray,
+    )
+    ii=0
+
+    thfineRes = thmin
+    thL=np.arange(-thmax,thmax+thfineRes,thfineRes,dtype=np.float64)
+    
+    
+    # X2=np.ascontiguousarray(X2)
+    for j in range(len(thL)):
+        th=thL[j]
+        R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
+        XX=np.transpose(R.dot(X22.T))
+        XX=np.transpose(H12mn[0:2,0:2].dot(XX.T))+H12mn[0:2,2]
+        Xth[th]=XX
+        
+        
+        for solbox in SolBoxes_init:
+            xs,ys,d0,d1 = solbox
+            Tj=np.array((d0,d1))
+            Oj = np.array((xs,ys))
+            cost2=getPointCost(H,dx,Xth[th],Oj,Tj)
+            h.append((-cost2-np.random.rand()/1e10,xs,ys,d0,d1,lvl,th))
+            
+    heapq.heapify(h)
+    
+    # print("len(heap) = ",len(h))
+
+    while(1):
+        (cost,xs,ys,d0,d1,lvl,th)=heapq.heappop(h)
+        mainSolbox = (cost,xs,ys,d0,d1,lvl,th)
+        if lvl==mxLVL:
+            break
+        
+        nlvl = int(lvl)+1
+        dx=dxs[nlvl]
+        H=HLevels[nlvl]
+        Tj=np.array((d0,d1))
+        Oj = np.array((xs,ys))
+
+        
+        
+        Xg=np.arange(Oj[0],Oj[0]+Tj[0],dx[0])
+        Yg=np.arange(Oj[1],Oj[1]+Tj[1],dx[1])
+        
+        d0,d1=dx[0],dx[1]
+        Tj=np.array((d0,d1))
+        
+        for xs in Xg[:2]:
+            for ys in Yg[:2]:
+                Oj = np.array((xs,ys))
+                cost3=getPointCost(H,dx,Xth[th],Oj,Tj) 
+                heapq.heappush(h,(-cost3-np.random.rand()/1e10,xs,ys,d0,d1,float(nlvl),th))
+
+    t=mainSolbox[1:3]
+    th = mainSolbox[6]
+    cost=-mainSolbox[0]
+    
+
+    
+    HcompR=np.identity(3)
+    R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
+    HcompR[0:2,0:2]=R
+    
+    Ht=np.identity(3)
+    Ht[0:2,2]=t
+    
+    # first rotation by HcompR or R
+    # then H12mn
+    # then translate by t
+    # Ht*H12mn*HcompR*X22v
+    H12comp=np.dot(Ht.dot(H12mn),HcompR)
+    H12comp[0:2,2]=H12comp[0:2,2]+mn_orig
+    
+    
+
+    
+    # H1=np.identity(3)
+    # H1[0:2,2]=-mn_orig
+    
+    # Hlmax=np.identity(3)
+    # Hlmax[0:2,2]=-LmaxOrig
+
+    
+    # H2=nplinalg.inv(H1)
+    # H2=H2.dot(Hcomp)
+    # H3=H2.dot(Hlmax)
+    # H4=H3.dot(H1)
+    # H12comp=H4.dot(H12)
+
+    H21comp=nplinalg.inv(H12comp)
+    
+    return H21comp
     
 
 @jit(int32[:,:](float64[:,:], float64[:], float64[:]),nopython=True, nogil=True,cache=True) 
--- testslamRoadDon.py
+++ testslamRoadDon.py
@@ -282,8 +282,232 @@ for i in range(mxlvl):
 
 #%% Real bin matching
 plt.close("all")
+import heapq
+numba_cache=True
+dtype=np.float64
 
-dxMatch=np.array([0.5,0.5])
+from numba.core import types
+from numba.typed import Dict
+float_2Darray = types.float64[:,:]
+
+def binMatcherAdaptive3(X11,X22,H12,Lmax,thmax,thmin,dxMatch):
+    #  X11 are global points
+    # X22 are points with respect to a local frame (like origin of velodyne)
+    # H12 takes points in the velodyne frame to X11 frame (global)
+    
+    n=histsmudge =2 # how much overlap when computing max over adjacent hist for levels
+    H21comp=np.identity(3)
+        
+    mn=np.zeros(2)
+    mx=np.zeros(2)
+    mn_orig=np.zeros(2)
+    mn_orig[0] = np.min(X11[:,0])
+    mn_orig[1] = np.min(X11[:,1])
+    
+    mn_orig=mn_orig-dxMatch
+    
+    
+    # R=H12[0:2,0:2]
+    # t=H12[0:2,2]
+    # X222 = R.dot(X22.T).T+t
+    
+    H12mn = H12.copy()
+    H12mn[0:2,2]=H12mn[0:2,2]-mn_orig
+    
+    # X2=X222-mn_orig
+    X1=X11-mn_orig
+    
+    
+    # t2 = np.mean(X2,axis=0)
+    # X20 = X2-t2
+    
+    mn[0] = np.min(X1[:,0])
+    mn[1] = np.min(X1[:,1])
+    mx[0] = np.max(X1[:,0])
+    mx[1] = np.max(X1[:,1])
+    # rmax=np.max(np.sqrt(X2[:,0]**2+X2[:,1]**2))
+
+    P = mx-mn
+
+    mxlvl=0
+    dx0=mx+dxMatch
+    dxs = []
+    XYedges=[]
+    for i in range(0,100):
+        f=2**i
+        
+        xedges=np.linspace(0,mx[0]+1*dxMatch[0],f+1)
+        yedges=np.linspace(0,mx[1]+1*dxMatch[0],f+1)
+        XYedges.append((xedges,yedges))
+        dx=np.array([xedges[1]-xedges[0],yedges[1]-yedges[0]])
+    
+        dxs.append(dx)
+        
+        if np.any(dx<=dxMatch):
+            break
+        
+    mxlvl=len(dxs)
+    
+
+    dxs=[dx.astype(np.float64) for dx in dxs]
+
+    
+    
+    H1match=nbpt2Dproc.numba_histogram2D(X1, XYedges[-1][0],XYedges[-1][1])
+    H1match = np.sign(H1match)
+    
+    
+    
+    
+
+    levels=[]
+    HLevels=[H1match]
+    
+    
+    
+    
+    
+    for i in range(1,mxlvl):
+        
+    
+    
+        Hup = HLevels[i-1]
+        H=nbpt2Dproc.UpsampleMax(Hup,n)
+
+        HLevels.append(H)
+          
+    
+    mxLVL=len(HLevels)-1
+    HLevels=HLevels[::-1]
+    HLevels=[np.ascontiguousarray(H).astype(np.int32) for H in HLevels]
+    
+    # P2=0.001*P
+    # Lmax=P2*(np.floor(Lmax/P2)+1)
+    # Lmax=np.maximum(Lmax,P)
+    
+    LmaxOrig=np.zeros(2,dtype=np.float64)
+    LmaxOrig[0]=Lmax[0]
+    LmaxOrig[1]=Lmax[1]
+
+    SolBoxes_init=[]
+    # X2[:,0]=X2[:,0]-LmaxOrig[0]
+    # X2[:,1]=X2[:,1]-LmaxOrig[1]
+    
+    # print(Lmax)
+    # Lmax=dxs[-1]*(np.floor(Lmax/dxs[-1])+1)
+    # print(Lmax)
+    for xs in np.arange(-Lmax[0],Lmax[0],dxs[0][0]):
+        for ys in np.arange(-Lmax[1],Lmax[1],dxs[0][1]):
+            SolBoxes_init.append( (xs,ys,dxs[0][0],dxs[0][1]) )
+    
+    
+    
+    
+    
+    
+    h=[(100000.0,0.0,0.0,0.0,0.0,0.0,0.0)]
+    lvl=0
+    dx=dxs[lvl]
+    H=HLevels[lvl]
+    
+    Xth= Dict.empty(
+        key_type=types.float64,
+        value_type=float_2Darray,
+    )
+    ii=0
+
+    thfineRes = thmin
+    thL=np.arange(-thmax,thmax+thfineRes,thfineRes,dtype=np.float64)
+    
+    
+    # X2=np.ascontiguousarray(X2)
+    for j in range(len(thL)):
+        th=thL[j]
+        R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
+        XX=np.transpose(R.dot(X22.T))
+        XX=np.transpose(H12mn[0:2,0:2].dot(XX.T))+H12mn[0:2,2]
+        Xth[th]=XX
+        
+        
+        for solbox in SolBoxes_init:
+            xs,ys,d0,d1 = solbox
+            Tj=np.array((d0,d1))
+            Oj = np.array((xs,ys))
+            cost2=nbpt2Dproc.getPointCost(H,dx,Xth[th],Oj,Tj)
+            h.append((-cost2-np.random.rand()/1e10,xs,ys,d0,d1,lvl,th))
+            
+    heapq.heapify(h)
+    
+    # print("len(heap) = ",len(h))
+
+    while(1):
+        (cost,xs,ys,d0,d1,lvl,th)=heapq.heappop(h)
+        mainSolbox = (cost,xs,ys,d0,d1,lvl,th)
+        if lvl==mxLVL:
+            break
+        
+        nlvl = int(lvl)+1
+        dx=dxs[nlvl]
+        H=HLevels[nlvl]
+        Tj=np.array((d0,d1))
+        Oj = np.array((xs,ys))
+
+        
+        
+        Xg=np.arange(Oj[0],Oj[0]+Tj[0],dx[0])
+        Yg=np.arange(Oj[1],Oj[1]+Tj[1],dx[1])
+        
+        d0,d1=dx[0],dx[1]
+        Tj=np.array((d0,d1))
+        
+        for xs in Xg[:2]:
+            for ys in Yg[:2]:
+                Oj = np.array((xs,ys))
+                cost3=nbpt2Dproc.getPointCost(H,dx,Xth[th],Oj,Tj) 
+                heapq.heappush(h,(-cost3-np.random.rand()/1e10,xs,ys,d0,d1,float(nlvl),th))
+
+    t=mainSolbox[1:3]
+    th = mainSolbox[6]
+    cost=-mainSolbox[0]
+    
+
+    
+    HcompR=np.identity(3)
+    R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
+    HcompR[0:2,0:2]=R
+    
+    Ht=np.identity(3)
+    Ht[0:2,2]=t
+    
+    # first rotation by HcompR or R
+    # then H12mn
+    # then translate by t
+    # Ht*H12mn*HcompR*X22v
+    H12comp=np.dot(Ht.dot(H12mn),HcompR)
+    H12comp[0:2,2]=H12comp[0:2,2]+mn_orig
+    
+    
+
+    
+    # H1=np.identity(3)
+    # H1[0:2,2]=-mn_orig
+    
+    # Hlmax=np.identity(3)
+    # Hlmax[0:2,2]=-LmaxOrig
+
+    
+    # H2=nplinalg.inv(H1)
+    # H2=H2.dot(Hcomp)
+    # H3=H2.dot(Hlmax)
+    # H4=H3.dot(H1)
+    # H12comp=H4.dot(H12)
+
+    H21comp=nplinalg.inv(H12comp)
+    
+    return H21comp
+    
+
+dxMatch=np.array([2,2])
 
 def down_sample(X,voxel_size):
     NN=X.ndim
@@ -298,24 +522,36 @@ def down_sample(X,voxel_size):
 idx=(X11[:,0]>50) & (X11[:,0]<125) & (X11[:,1]>-50) & (X11[:,1]<50)
 
 X2=X11[idx,:]
-th=5*np.pi/180
-R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
-
-X2=R.dot(X2.T).T+50
 X2=down_sample(X2,dxMatch[0])
+
+tm=np.mean(X2,axis=0)
+X2=X2-tm
+H12est=np.identity(3)
+H12est[0:2,2]=tm+25
+
+th=-15*np.pi/180
+R = np.array([[np.cos(th), -np.sin(th)],[np.sin(th), np.cos(th)]])
+X2=R.dot(X2.T).T
+
+X2g=H12est[0:2,0:2].dot(X2.T).T+H12est[0:2,2]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(X11[:,0],X11[:,1],'k.')
-ax.plot(X2[:,0],X2[:,1],'b.')
+ax.plot(X2g[:,0],X2g[:,1],'b.')
 
 
-H12est=np.identity(3)
-Lmax=np.array([510,510])
-thmax=10*np.pi/180
-thmin=1*np.pi/180
-Hbin21=nbpt2Dproc.binMatcherAdaptive3(X11,X2,H12est,Lmax,thmax,thmin,dxMatch)
- 
+
+Lmax=np.array([30,30])
+thmax=20*np.pi/180
+thmin=5*np.pi/180
+st=time.time()
+# Hbin21=binMatcherAdaptive3(X11,X2,H12est,Lmax,thmax,thmin,dxMatch)
+Hbin21=nbpt2Dproc.binMatcherAdaptive4_good(X11,X2,H12est,Lmax,thmax,thmin,dxMatch)
+et=time.time()
+print("time for bin match = ",et-st)
+# nbpt2Dproc.
+
 Hbin12=nplinalg.inv(Hbin21) 
 X2t=Hbin12[0:2,0:2].dot(X2.T).T+Hbin12[:2,2]
 
